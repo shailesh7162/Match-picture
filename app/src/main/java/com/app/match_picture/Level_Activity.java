dfdce2bec04b807f2ad89df3fd46d01d07196eb9
DELL <shaileshmakwana8000@gmail.com>
2023-04-27 18:57:46 -0700

    4

--- app/src/main/java/com/app/match_picture/Level_Activity.java
+++ app/src/main/java/com/app/match_picture/Level_Activity.java
@@ -13,15 +13,22 @@ public class Level_Activity extends AppCompatActivity
 {
     ImageView infobtn;
     RecyclerView levelRecycal;
-
-
+    Level_Adpter level_adpter;
+    int i=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
         infobtn=findViewById(R.id.infobtn);
         levelRecycal=findViewById(R.id.levelRecycal);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Select level");
+        int i=getIntent().getIntExtra("key",0);
+        if (i==0)
+        {
+            getSupportActionBar().setSubtitle("NO TIME LIMIT");
 
+        }
 
         infobtn.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/app/match_picture/Level_Adpter.java
+++ app/src/main/java/com/app/match_picture/Level_Adpter.java
@@ -1,28 +1,59 @@
 package com.app.match_picture;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class Level_Adpter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
+public class Level_Adpter extends RecyclerView.Adapter<Level_Adpter.ViewHolder>
 {
-    RecyclerView recyclerView;
+    Context context;
+    String[] number={"1","2","3","4","5","6","7","8","9","10"};
+    int i;
+
+    public Level_Adpter(Context context,int i)
+    {
+        this.context=context;
+        this.i=i;
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public Level_Adpter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        return new
+         ViewHolder(LayoutInflater.from(context).inflate(R.layout.level_iteam,parent,false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull Level_Adpter.ViewHolder holder, int position)
+    {
+        holder.level_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(context,Game_Activity.class);
+                intent.putExtra("Level_type",i);
+                intent.putExtra("pos",(holder.getAdapterPosition()+1));
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return number.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        Button level_btn;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            level_btn=itemView.findViewById(R.id.level_btn);
+        }
     }
 }
