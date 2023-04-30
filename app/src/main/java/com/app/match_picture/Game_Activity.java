df8f6e78a4f808cc7c97d530ff6e6cb2c3ca0ab1
DELL <shaileshmakwana8000@gmail.com>
2023-04-30 03:33:15 -0700

    5

--- app/src/main/java/com/app/match_picture/Game_Activity.java
+++ app/src/main/java/com/app/match_picture/Game_Activity.java
@@ -1,14 +1,70 @@
 package com.app.match_picture;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class Game_Activity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game_Activity extends AppCompatActivity
+{
+    RecyclerView recyclerView;
+    Recycle_Adapter recycle_adapter;
+    SeekBar seekBar;
+    TextView time;
+    List<Integer> list1=new ArrayList<>();
+    List<Integer> list2=new ArrayList<>();
+    List<Integer> imglist=new ArrayList<>();
+    Handler handler;
+    Runnable runnable;
+    int i,l,max;
+    public static final int[] IMAGES={R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,R.drawable.f,R.drawable.g,R.drawable.h,R.drawable.i,R.drawable.j};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        time = findViewById(R.id.time);
+        recyclerView = findViewById(R.id.recycal);
+
+        l = getIntent().getIntExtra("pos", 0);
+        i = getIntent().getIntExtra("level_type", 0);
+
+        if (l <= 3) {
+            for (int j = 0; j < 12; j++) {
+                imglist.add(j);
+            }
+        } else if (l <= 6) {
+            for (int j = 0; j < 16; j++) {
+                imglist.add(j);
+            }
+        } else {
+            for (int j = 0; j < 20; j++) {
+                imglist.add(j);
+            }
+        }
+
+        seekBar = findViewById(R.id.seekbar );
+
+        if (i == 1) {
+            getSupportActionBar().setTitle("NO TIME LIMIT");
+            seekBar.setVisibility(View.GONE);
+        } else if (i == 2) {
+            getSupportActionBar().setTitle("NORMAL");
+            seekBar.setVisibility(View.VISIBLE);
+        } else if (i == 3) {
+            getSupportActionBar().setTitle("HARD");
+            seekBar.setVisibility(View.VISIBLE);
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/app/match_picture/Recycle_Adapter.java
@@ -0,0 +1,126 @@
+package com.app.match_picture;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Recycle_Adapter extends RecyclerView.Adapter<Recycle_Adapter.ViewHolder>
+{
+    Activity activity;
+    List<Integer> img;
+    int click = 1;
+    View view1;
+    int pos1, pos2;
+    int check;
+
+    public Recycle_Adapter(Activity activity,List<Integer> img)
+    {
+        this.activity=activity;
+        this.img=img;
+    }
+
+    @NonNull
+    @Override
+    public Recycle_Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(activity).inflate(R.layout.recycel_itam,parent,false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Recycle_Adapter.ViewHolder holder, int position) {
+        holder.img.setImageResource(img.get(position));
+        holder.cover.setVisibility(View.INVISIBLE);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                holder.cover.setVisibility(View.VISIBLE);
+            }
+        }, 4000);
+
+        holder.cover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (click==1)
+                {
+                    holder.cover.setVisibility(View.INVISIBLE);
+                    click=3;
+                    view1=holder.cover;
+                    pos1=holder.getAdapterPosition();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            click = 2;
+                        }
+                    }, 100);
+                }else if (click==2)
+                {
+                    holder.cover.setVisibility(View.INVISIBLE);
+                    click=3;
+                    pos2=holder.getAdapterPosition();
+
+                    if (img.get(pos1).equals(img.get(pos2)))
+                    {
+                        holder.cover.setClickable(false);
+                        view1.setClickable(false);
+                        click=1;
+                        check++;
+                        boolean plus = true;
+                        if (check==(img.size()/2))
+                        {
+                            boolean win = true;
+                            Dialog dialog=new Dialog(activity);
+                            dialog.setCancelable(false);
+                            dialog.setContentView(R.layout.win_dialog);
+
+                            Button win_okbtn=dialog.findViewById(R.id.okbtn);
+                            win_okbtn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    dialog.dismiss();
+                                    activity.finish();
+                                }
+                            });
+                            dialog.show();
+                        }
+                    }else
+                    {
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                holder.cover.setVisibility(View.VISIBLE);
+                                view1.setVisibility(View.VISIBLE);
+                                click = 1;
+                            }
+                        }, 500);
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return img.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        ImageView img,cover;
+        public ViewHolder(View itemViwe) {
+            super(itemViwe);
+            img=itemViwe.findViewById(R.id.img);
+            cover=itemViwe.findViewById(R.id.cover);
+        }
+    }
+}
